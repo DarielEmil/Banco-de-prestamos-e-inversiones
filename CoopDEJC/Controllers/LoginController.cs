8dd5509cc0ff35cf2a58dde313b66b01be01e6c3
CaesaRR19 <coramosnolasco@gmail.com>
2023-04-06 15:06:55 -0400

    Add Login

---
+++ CoopDEJC/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoopDEJC.Models;
+namespace CoopDEJC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User _user)
+        {
+            if (_user.Clave == _user.ConfirmarClave)
+            {
+                //Encriptar _user.clave (si es que se va a hacer)
+            }
+            else
+            {
+                ViewBag.msg = "Las contraseñas no coinciden";
+                return View();
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User _user)
+        {
+            return View();
+        }
+    }
+}

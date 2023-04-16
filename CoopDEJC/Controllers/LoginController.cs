f000692e63e2c987e2366bbff24f323eecf7a87a
RyukTV <97322176+RyukTV@users.noreply.github.com>
2023-04-16 02:29:13 -0400

    pulltojesus

--- CoopDEJC/Controllers/LoginController.cs
+++ CoopDEJC/Controllers/LoginController.cs
@@ -51,46 +51,6 @@ namespace CoopDEJC.Controllers
             return View();
         }
 
-        //Autenticador
-        [HttpPost]
-        public IActionResult Login1(string email, string password)
-        {
-
-            /*Cliente objeto = new LO_Usuario().SearchUser(email, password);
-          
-            if (objeto.Nombre == )
-            {
-                
-                    return RedirectToAction("Index", "Home");
-            }
-            
-            /*
-              {
-                  using (CoopContext db = new CoopContext()) 
-
-                  {
-                      var usuario = from c in db.Clientes 
-                                    where c.Correo == email && c.Clave == password 
-                                    select c;
-                      if (usuario.Count() > 0)
-                      {
-                          return RedirectToAction("Index", "Home");
-                      }else
-                    {
-                        
-                        return Content("Usuario invalido ");                    
-                    }
-                  }
-
-              }
-              catch(Exception ex)
-              {
-
-              }*/
-
-            return View();
-        }
-
 
         //[HttpPost]
         //public IActionResult Register(User _user)
